81c2e5a71da0f902056bd8f3d7c03cdd116a2bdc
Michael Luton <mluton@gmail.com>
2012-01-22 21:43:44 -0800

    Initial Commit

---
+++ SegmentedControlViews/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SegmentedControlViews
+//
+//  Created by Michael Luton on 1/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SegmentedControlViews/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SegmentedControlViews
+//
+//  Created by Michael Luton on 1/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
